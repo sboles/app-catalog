cb9d5aa2ee4aaca51f4e4f13302b2a2ad32e4d1d
Doug Diehnelt <ddiehnelt@rallydev.com>
2014-02-07 09:42:51 -0700

    Parent field names now an object not an array

--- src/apps/iterationtrackingboard/IterationTrackingBoardApp.js
+++ src/apps/iterationtrackingboard/IterationTrackingBoardApp.js
@@ -191,16 +191,23 @@
                 Ext.apply(gridConfig, {
                     xtype: 'rallytreegrid',
                     model: treeGridModel,
+                    parentFieldNames: {
+                        defect: ['Requirement', 'DefectSuite'],
+                        task: ['WorkProduct']
+                    },
                     storeConfig: {
                         nodeParam: 'Parent',
-                        parentFieldNames: ['Requirement', 'WorkProduct', 'DefectSuite'],
+                        // parentFieldNames: ['Requirement', 'WorkProduct', 'DefectSuite'],
                         parentTypes: ['HierarchicalRequirement', 'Defect', 'DefectSuite', 'TestSet'],
                         childTypes: ['Defect', 'Task', 'TestCase'],
                         rootNodeFilters: this.context.getTimeboxScope().getQueryFilter(),
-                        sorters: {
+                        sorters: [{
                             property: Rally.data.Ranker.getRankField(treeGridModel),
                             direction: 'ASC'
-                        },
+                        },{
+                            property: 'TaskIndex',
+                            direction: 'ASC'
+                        }],
                         fetch: ['FormattedID', 'Tasks', 'Defects', 'TestCases']
                     },
                     treeColumnRenderer: function(value, metaData, record, rowIdx, colIdx, store, view) {
